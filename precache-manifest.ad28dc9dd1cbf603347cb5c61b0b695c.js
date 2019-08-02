6e5710872e498de8b8a896122fd35d48eef0a522
Zenderg <danil54658798@gmail.com>
2019-08-02 15:19:30 +0300

    Add files via upload

---
+++ precache-manifest.ad28dc9dd1cbf603347cb5c61b0b695c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e6ba9278fb680e93f16b3472b7cd64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04f29e4dcd79f4aa07b5",
+    "url": "/static/css/main.a74cb01d.chunk.css"
+  },
+  {
+    "revision": "0cb3f275bb5eeea5e6e4",
+    "url": "/static/js/2.aca22a71.chunk.js"
+  },
+  {
+    "revision": "04f29e4dcd79f4aa07b5",
+    "url": "/static/js/main.301763e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
